07a30c64e6dcb6b7286ad912b564cad509887102
Matheus Artur Cabral Costa <macc@ic.ufal.br>
2018-09-11 17:39:43 -0300

    less lines

--- src/main.c
+++ src/main.c
@@ -9,43 +9,26 @@ UFAL
 #include <string.h>
 #include "hufflist.h"
 #include "hufftree.h"
-
 #define DEBUG 0
 
-// Function to compress a file.
 void compress(FILE* file) {
-
-	// Pointer to the linked list
 	huff_node *frequency_list = NULL;
+	huff_node * huffman_tree = NULL;
 
-	// Pointer to the huffman tree root
-	huff_node *huffman_tree = NULL;
-
-	// Create a frequency list from the bytes in the file
+	// Create a frequency list from the bytes in the file and sort it.
 	create_frequency_list(file, &frequency_list);
-
-	// Sort the frequency list
 	sort_frequency_list(&frequency_list);
 
 	// Debug loop to print the bytes in hexadecimal and their frequencies.
 	huff_node *aux_list = frequency_list;
 	while(aux_list != NULL && DEBUG == 1) {
 		printf("BYTE: %hhx / FREQ: %d\n", *((char *)aux_list->value), aux_list->freq);
-		// printf("BYTE: %c / FREQ: %d\n", *((char *)aux_list->value), aux_list->freq);
 		aux_list = aux_list->next;
 	}
 	puts("");
 
 	// Create the file's huffman tree
 	build_huffman_tree(&huffman_tree, frequency_list);
-
-	// Debug loop to print the tree
-	// if (DEBUG == 1) {
-		print_tree(huffman_tree, 0);
-	// }
-
-	// Build the compressed file 
-	// build_compressed_file(huffman_tree, );
 
 	puts("");
 }
@@ -55,15 +38,11 @@ int main(int argc, char **argv) {
 	The command that will determine the operation to be executed is passed as the first argument (argv[1]).
 	the available command/operation touples are: (-c, compress), (-e, extract) and (-h, help).
 	The file that will be used is passed as the second argument (argv[2]) when the program is executed. 
-	
-	An example of the usage is: ./program -c picture.jpg, that will compress the file "picture.jpg"
 
+	An example of the usage is: ./program -c picture.jpg, that will compress the file "picture.jpg"
 	The file specified will be stored in the variable "file", as read-only, to ensure that the original data is not
 	altered in the compression process. 
 	*/
-
-	system("clear");
-	
 	if(argv[1] == NULL || strcmp(argv[1], "-h") == 0) {
 		printf("\nThe correct syntax to use the program is: ./program <command> <file>\n");
 		printf("Available commands are:\n");
@@ -77,7 +56,6 @@ int main(int argc, char **argv) {
 			file = fopen(argv[2], "r");
 		}
 		if(file == NULL) {
-			// If the file cannot be accessed, the program exits with error code 1.
 			printf("Error! File could not be loaded successfully.\nExiting...\n");
 			return 1;
 		}
@@ -90,15 +68,12 @@ int main(int argc, char **argv) {
 			file = fopen(argv[2], "r");
 		}
 		if(file == NULL) {
-			// If the file cannot be accessed, the program exits with error code 1.
 			printf("Error! File could not be loaded successfully.\nExiting...\n");
 			return 1;
 		}
 		char *current_byte = malloc(sizeof(char *));
-		//fread will read one byte at a time from the "file" variable and store it in the "current_byte" variable.
 		while(fread(current_byte, 1, 1, file)) {
 			//TODO: extraction algorithm.
-			//magic happens here.
 		}
 		fclose(file);
 	}
