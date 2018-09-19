87e71e4ce2f36709e689293469b2cb14cf609173
Nicolas Leão <niicolasleao@gmail.com>
2018-09-19 20:47:10 -0300

    Updated extract.h

--- src/extract.h
+++ src/extract.h
@@ -3,8 +3,9 @@
 #include <math.h>
 #include "hufflist.h"
 #include "hufftree.h"
+#include <stdbool.h>
 
-int is_bit_set(unsigned char c, int i) {
+bool get_bit(unsigned char c, int i) {
 	unsigned char aux = 1 << i;
 	return aux & c;
 }
@@ -13,14 +14,22 @@ void extract(FILE* file) {
 	/* The two first bytes of the file provides us the information needed to build the tree that will be used */
 	unsigned char header[2];
 	int thrash = 0, tree_size = 0;
-	fscanf(file, "%c%c", header[0], header[1]);
-	for(int i = 3; i > 0; --i) {
-	  thrash += is_bit_set(header[0], i)*pow(2, i);
+	fscanf(file, "%c%c", &header[0], &header[1]);
+
+	// Three fors to organize the reading of the header values.
+	for(int i = 5; i < 8; ++i) {
+	  thrash += get_bit(header[0], i)*pow(2, i);
 	}
-	for(int i = 8; i > 0; --i) {
-	  tree_size += is_bit_set(header[1], i)*pow(2, i);
+	for(int i = 0; i < 8; ++i) {
+		tree_size += get_bit(header[1], i)*pow(2, i);
 	}
-	printf("%d\n", thrash);
+	for(int i = 0; i < 5; ++i) {
+		tree_size += get_bit(header[0], i)*pow(2, 8+i);
+	}
+
+	DEBUG {
+		printf("THRASH: %d TREE_SIZE: %d\n", thrash, tree_size);
+	}
 }
 
 #endif
