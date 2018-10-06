be2f3b5bd3b574691a36a99309e5310ea3cea577
Matheus Artur Cabral Costa <macc@ic.ufal.br>
2018-10-05 21:02:44 -0300

    segment tree done

--- src/segtree.c
+++ src/segtree.c
@@ -2,18 +2,6 @@
 #include <stdlib.h>
 
 int
-sum_leaf(int *v, int i, int proc)
-{
-  return v[i];
-}
-
-int
-min_max_leaf(int *v, int i, int proc)
-{
-  return i;
-}
-
-int
 min_tree(int l, int r)
 {
   if(l > r)
@@ -38,20 +26,20 @@ sum(int l, int r)
 }
 
 void
-buildtree(int (*f)(int l, int r), int (*leaf)(int *v, int i, int proc),
-	  int *v, int *tree, int *t_size, int key, int min, int max)
+buildtree(int (*f)(int l, int r), int *v, int *tree,
+	  int *t_size, int key, int min, int max)
 {
   int mid;
   
   if(min == max)
-    //    tree[key] = (*leaf)(v, min, key);
-    tree[key] = max;
+    tree[key] = v[min];
+
   else
     {
       mid = (min+max)/2;
       
-      buildtree((*f), (*leaf), v, tree, t_size, 2*key+1, min , mid);
-      buildtree((*f), (*leaf), v, tree, t_size, 2*key+2, mid+1 , max);
+      buildtree((*f), v, tree, t_size, 2*key+1, min , mid);
+      buildtree((*f), v, tree, t_size, 2*key+2, mid+1 , max);
       tree[key] = (*f)(tree[2*key +1], tree[2*key + 2]);
     }
 }
@@ -73,8 +61,9 @@ main(void)
       scanf("%d", &v[i]);
     }
   
-  //  buildtree(sum, sum_leaf, v, tree, &t_size, 0, 0, v_size-1);
-  buildtree(min_tree, min_max_leaf, v, tree, &t_size, 0, 0, v_size-1);
+  buildtree(sum, v, tree, &t_size, 0, 0, v_size-1);
+  //  buildtree(min_tree, v, tree, &t_size, 0, 0, v_size-1);
+  //  buildtree(max_tree, v, tree, &t_size, 0, 0, v_size-1);
   
   for(i=0;i<t_size;++i)
     {
